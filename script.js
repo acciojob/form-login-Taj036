99cc4052b667d48940e050396ade79e50e5efa7c
Taj036 <121137573+Taj036@users.noreply.github.com>
2023-12-19 14:46:53 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+ let x = document.getElementsByName('fname')[0].value;
+ let y = document.getElementsByName('lname')[0].value;
+   alert(x +' ' + y );
 }
